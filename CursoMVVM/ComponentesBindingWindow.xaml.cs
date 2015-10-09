ea2b7989f01def423e4a6d4b6d9ef16e612ef380
Valerio Ferreira <valerio@prosiga.com.br>
2015-10-09 10:58:39 -0300

    Release inicial e ComponenteBindingWindow.

---
+++ CursoMVVM/ComponentesBindingWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace CursoMVVM
+{
+    /// <summary>
+    /// Interaction logic for ComponentesBindingWindow.xaml
+    /// </summary>
+    public partial class ComponentesBindingWindow
+    {
+        public ComponentesBindingWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
